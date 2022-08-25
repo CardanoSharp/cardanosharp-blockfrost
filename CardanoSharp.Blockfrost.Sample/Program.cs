65b01f00f467ed6a17dcfe67def5458300498abe
Paul Cowan <paul.cowan@gmail.com>
2022-08-25 21:55:28 +0200

    adding blocksclient and get latest block method and added to test program

--- CardanoSharp.Blockfrost.Sample/Program.cs
+++ CardanoSharp.Blockfrost.Sample/Program.cs
@@ -1,34 +1,34 @@
 ﻿using CardanoSharp.Blockfrost.Sdk;
 using CardanoSharp.Blockfrost.Sdk.Common;
-using Microsoft.Extensions.Hosting;
-using Refit;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((hostContext, services) =>
-    {
-        var apiKey = hostContext.Configuration["Blockfrost:ApiKey"];
-        var baseUrl = hostContext.Configuration["Blockfrost:BaseUrl"];
-        var authConfig = new AuthHeaderConfiguration(apiKey, baseUrl);
-        services.AddBlockfrost(authConfig);
+	.ConfigureServices((hostContext, services) =>
+	{
+		var apiKey = hostContext.Configuration["Blockfrost:ApiKey"];
+		var baseUrl = hostContext.Configuration["Blockfrost:BaseUrl"];
+		var authConfig = new AuthHeaderConfiguration(apiKey, baseUrl);
+		services.AddBlockfrost(authConfig);
 
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+		services.AddHostedService<Worker>();
+	})
+	.Build();
 await host.RunAsync();
 
 public class Worker : BackgroundService
 {
-    private readonly INetworkClient? _networkClient;
+	private readonly INetworkClient? _networkClient;
+	private readonly IBlocksClient? _blocksClient;
 
-    public Worker(INetworkClient? networkClient)
-    {
-        _networkClient = networkClient;
-    }
+	public Worker(INetworkClient? networkClient, IBlocksClient blocksClient)
+	{
+		_networkClient = networkClient;
+		_blocksClient = blocksClient;
+	}
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        // Network Calls
-        var network = await _networkClient.GetNetworkInformation();
-
-    }
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		// Network Calls
+		var network = await _networkClient.GetNetworkInformation();
+		var latestBlock = await _blocksClient.GetLatestBlock();
+	}
 }
---
+++ CardanoSharp.Blockfrost.Sdk/Contracts/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CardanoSharp.Blockfrost.Sdk.Contracts;
+
+public class Block
+{
+	[JsonPropertyName("time")] public ulong Time { get; set; }
+
+	[JsonPropertyName("height")] public uint Height { get; set; }
+
+	[JsonPropertyName("hash")] public string Hash { get; set; }
+
+	[JsonPropertyName("slot")] public uint Slot { get; set; }
+
+	[JsonPropertyName("epoch")] public uint Epoch { get; set; }
+
+	[JsonPropertyName("epoch_slot")] public uint EpochSlot { get; set; }
+
+	[JsonPropertyName("slot_leader")] public string SlotLeader { get; set; }
+
+	[JsonPropertyName("size")] public uint Size { get; set; }
+
+	[JsonPropertyName("tx_count")] public uint TxCount { get; set; }
+
+	[JsonPropertyName("output")] public string Output { get; set; }
+
+	[JsonPropertyName("fees")] public string Fees { get; set; }
+
+	[JsonPropertyName("block_vrf")] public string BlockVrf { get; set; }
+
+	[JsonPropertyName("op_cert")] public string OpCert { get; set; }
+
+	[JsonPropertyName("op_cert_counter")] public string OpCertCounter { get; set; }
+
+	[JsonPropertyName("previous_block")] public string PreviousBlock { get; set; }
+
+	[JsonPropertyName("next_block")] public string NextBlock { get; set; }
+
+	[JsonPropertyName("confirmations")] public uint Confirmations { get; set; }
+}

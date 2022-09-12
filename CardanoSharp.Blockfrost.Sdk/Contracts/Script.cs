b639147a06ac16c67ae5dd2084a8f9b565208822
Paul Cowan <paul.cowan@inversion.dev>
2022-09-12 16:32:27 +0200

    adding get script details method and contract

---
+++ CardanoSharp.Blockfrost.Sdk/Contracts/Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CardanoSharp.Blockfrost.Sdk.Contracts
+{
+	public class Script
+	{
+		[JsonPropertyName("script_hash")] public string ScriptHash { get; set; }
+
+		[JsonPropertyName("type")] public string Type { get; set; }
+
+		[JsonPropertyName("serialised_size")] public uint? SerialisedSize { get; set; }
+	}
+}
--- CardanoSharp.Blockfrost.Sdk/IScriptsClient.cs
+++ CardanoSharp.Blockfrost.Sdk/IScriptsClient.cs
@@ -7,4 +7,7 @@ public interface IScriptsClient
 {
 	[Get("/scripts/{script_hash}/json")]
 	Task<ApiResponse<ScriptJson>> GetScriptJsonAsync(string script_hash);
+
+	[Get("/scripts/{script_hash}")]
+	Task<ApiResponse<Script>> GetScriptAsync(string script_hash);
 }

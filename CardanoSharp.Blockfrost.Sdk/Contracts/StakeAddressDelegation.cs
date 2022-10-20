90d6e6b1b9dd8fcf1f3033a77c38600c0c578ed8
Paul Cowan <paul.cowan@inversion.dev>
2022-10-20 23:59:44 +0200

    adding staking distribution calls to epoch client (tested)

---
+++ CardanoSharp.Blockfrost.Sdk/Contracts/StakeAddressDelegation.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CardanoSharp.Blockfrost.Sdk.Contracts;
+
+public class StakeAddressDelegation
+{
+	[JsonPropertyName("stake_address")] public string StakeAddress { get; set; }
+
+	[JsonPropertyName("amount")] public string Amount { get; set; }
+}

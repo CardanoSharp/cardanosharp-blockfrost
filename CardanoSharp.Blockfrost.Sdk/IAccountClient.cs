e7d98331e489dd72c5077ab38aba9d6bf9c10869
Paul Cowan <paul.cowan@inversion.dev>
2022-11-08 12:38:11 +0200

    adding get account history (delegation)

---
+++ CardanoSharp.Blockfrost.Sdk/IAccountClient.cs
@@ -0,0 +1,10 @@
+﻿using CardanoSharp.Blockfrost.Sdk.Contracts;
+using Refit;
+
+namespace CardanoSharp.Blockfrost.Sdk;
+
+public interface IAccountClient
+{
+	[Get("/accounts/{stake_address}/delegations?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<EpochPoolTxDelegation[]>> GetAccountDelegationHistory(string stake_address, int count = 100, int page = 1, string order = "asc");
+}

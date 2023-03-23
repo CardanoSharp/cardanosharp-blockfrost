7f6553afb8d972f3317c7dc2951343eb41c2a2b9
Paul Cowan <paul.cowan@inversion.dev>
2023-03-23 23:37:49 +0200

    adding get account rewards

---
+++ CardanoSharp.Blockfrost.Sdk/Contracts/AccountReward.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CardanoSharp.Blockfrost.Sdk.Contracts;
+
+public class AccountReward
+{
+	/// <summary>
+	/// Epoch of the associated reward
+	/// </summary>
+	[JsonPropertyName("epoch")] public uint Epoch { get; set; }
+
+	/// <summary>
+	/// Rewards for given epoch in Lovelaces
+	/// </summary>
+	[JsonPropertyName("amount")] public string Amount { get; set; }
+
+	/// <summary>
+	/// Bech32 pool ID being delegated to
+	/// </summary>
+	[JsonPropertyName("pool_id")] public string PoolId { get; set; }
+
+	/// <summary>
+	/// Type of the reward ["leader"/"member"/"pool_deposit_refund"]
+	/// </summary>
+	[JsonPropertyName("type")] public string Type { get; set; }
+}
--- CardanoSharp.Blockfrost.Sdk/IAccountClient.cs
+++ CardanoSharp.Blockfrost.Sdk/IAccountClient.cs
@@ -5,18 +5,21 @@ namespace CardanoSharp.Blockfrost.Sdk;
 
 public interface IAccountClient
 {
-    [Get("/accounts/{stake_address}/delegations?count={count}&page={page}&order={order}")]
-    Task<ApiResponse<EpochPoolTxDelegation[]>> GetAccountDelegationHistory(string stake_address, int count = 100, int page = 1, string order = "asc");
+	[Get("/accounts/{stake_address}/delegations?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<EpochPoolTxDelegation[]>> GetAccountDelegationHistory(string stake_address, int count = 100, int page = 1, string order = "asc");
 
-    [Get("/accounts/{stake_address}/history?count={count}&page={page}&order={order}")]
-    Task<ApiResponse<EpochPoolDelegation[]>> GetAccountHistory(string stake_address, int count = 100, int page = 1, string order = "asc");
+	[Get("/accounts/{stake_address}/history?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<EpochPoolDelegation[]>> GetAccountHistory(string stake_address, int count = 100, int page = 1, string order = "asc");
 
-    [Get("/accounts/{stake_address}/addresses?count={count}&page={page}&order={order}")]
-    Task<ApiResponse<AddressBasic[]>> GetAccountAssociatedAddresses(string stake_address, int count = 100, int page = 1, string order = "asc");
+	[Get("/accounts/{stake_address}/addresses?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<AddressBasic[]>> GetAccountAssociatedAddresses(string stake_address, int count = 100, int page = 1, string order = "asc");
 
-    [Get("/accounts/{stake_address}/addresses/assets?count={count}&page={page}&order={order}")]
-    Task<ApiResponse<UnitQuantity[]>> GetAccountAssociatedAddressesAssets(string stake_address, int count = 100, int page = 1, string order = "asc");
+	[Get("/accounts/{stake_address}/addresses/assets?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<UnitQuantity[]>> GetAccountAssociatedAddressesAssets(string stake_address, int count = 100, int page = 1, string order = "asc");
 
-    [Get("/accounts/{stake_address}")]
-    Task<ApiResponse<AccountDetails>> GetAccountDetails(string stake_address);
+	[Get("/accounts/{stake_address}")]
+	Task<ApiResponse<AccountDetails>> GetAccountDetails(string stake_address);
+
+	[Get("/accounts/{stake_address}/rewards?count={count}&page={page}&order={order}")]
+	Task<ApiResponse<AccountReward[]>> GetAccountRewards(string stake_address, int count = 100, int page = 1, string order = "asc");
 }
